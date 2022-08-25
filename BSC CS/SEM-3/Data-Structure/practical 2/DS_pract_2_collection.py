ed69042726f9157e463bd4c86915940c75d94432
varaliyamohammed8097 <varaliyamohammed1@gmail.com>
2022-08-25 18:00:56 +0530

    added data structure practical

---
+++ BSC CS/SEM-3/Data-Structure/practical 2/DS_pract_2_collection.py
@@ -0,0 +1,20 @@
+import collections
+
+de = collections.deque([1, 2, 3])
+print("deque: ", de)
+
+de.append(4)
+
+print("\nThe deque after appending at right is : ", de)
+
+de.appendleft(6)
+
+print("\nThe deque after appending at left is : ", de)
+
+de.pop()
+
+print("\nThe deque after deleting from right is : ", de)
+
+de.popleft()
+
+print("\nThe deque after deleting from left is : ", de)
