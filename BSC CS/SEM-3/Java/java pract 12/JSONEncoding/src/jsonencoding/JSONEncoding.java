f98c8a854047017afef61e3cebf763d211d692f9
varaliyamohammed8097 <varaliyamohammed1@gmail.com>
2022-08-27 15:46:44 +0530

    Add java Practical 12

---
+++ BSC CS/SEM-3/Java/java pract 12/JSONEncoding/src/jsonencoding/JSONEncoding.java
@@ -0,0 +1,26 @@
+package jsonencoding;
+
+import java.util.*;
+import org.json.*;
+
+public class JSONEncoding {
+
+    
+    public static void main(String[] args) throws JSONException {
+        
+        // JSON Encoding
+        JSONObject json = new JSONObject();
+        json.put("1", "varaliya");
+        json.put("2", "amaan");
+        json.put("3", "sahil");
+        json.put("4", "sameer");
+        json.put("5", "taha");
+        
+        System.out.println("Json Encoding"+ json);
+        
+        // JSON Decoding 
+            
+        
+    }
+    
+}
