730bfe69d5c5b88c82f25154356210e06e73a60b
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-08-30 00:13:51 +0530

    Add practical 3 wuestion 3 statistic Fy

---
+++ BSC CS/FY/SEM-1/Descriptive Statistics/practical-3/Matrix-Function.R
@@ -0,0 +1,16 @@
+# Use matrix function to create the matrix for data set
+
+mymat = matrix(1:12,4,3)
+mymat
+
+# Example 1
+mymat = matrix(1:9,3,3)
+mymat
+
+#Example 2
+mymat = matrix(c(4,3,30,28),2,2)
+mymat
+
+#Example 3
+mymat = matrix(c(5),1,1)
+mymat
