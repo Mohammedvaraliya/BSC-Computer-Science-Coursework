8108b9386a6fc77539b8109706d8951fe61554c6
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-08-29 23:58:01 +0530

    Add practical 3 statistics FY

--- BSC CS/FY/SEM-1/Descriptive Statistics/practical-3/Mathematical-Operator.R
+++ BSC CS/FY/SEM-1/Descriptive Statistics/practical-3/Mathematical-Operator.R
@@ -1,6 +1,24 @@
 print("Varaliya Mohammed")
+
+# Example 1
 x <- 5
 y <- 2
+z1 <- x*y
+z2 <- x/y
+z3 <- x^y
+z1; z2; z3
+
+#Example 2
+x <- 22.4
+y <- 12.5
+z1 <- x*y
+z2 <- x/y
+z3 <- x^y
+z1; z2; z3
+
+#Example 3
+x <- 33.4
+y <- 32.1
 z1 <- x*y
 z2 <- x/y
 z3 <- x^y
