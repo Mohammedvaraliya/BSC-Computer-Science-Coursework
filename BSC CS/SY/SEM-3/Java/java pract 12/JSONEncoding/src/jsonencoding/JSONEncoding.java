139eb8acc14baefc1609a9066a198d8f078c1894
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-08-30 23:44:37 +0530

    Add java pract 12

--- BSC CS/SY/SEM-3/Java/java pract 12/JSONEncoding/src/jsonencoding/JSONEncoding.java
+++ BSC CS/SY/SEM-3/Java/java pract 12/JSONEncoding/src/jsonencoding/JSONEncoding.java
@@ -1,13 +1,13 @@
 package jsonencoding;
 
 import java.util.*;
-import org.json.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class JSONEncoding {
 
-    
-    public static void main(String[] args) throws JSONException {
-        
+    public static void main(String[] args) {
+
         // JSON Encoding
         JSONObject json = new JSONObject();
         json.put("1", "varaliya");
@@ -15,12 +15,21 @@ public class JSONEncoding {
         json.put("3", "sahil");
         json.put("4", "sameer");
         json.put("5", "taha");
-        
-        System.out.println("Json Encoding"+ json);
-        
-        // JSON Decoding 
-            
-        
+
+        System.out.println("Json Encoding" + json);
+
+        // JSON Decoding
+        String s = "{\"1\":\"varaliya\",\"2\":\"amaan\",\"3\":\"sahil\",\"4\":\"sameer\",\"5\":\"taha\"}";
+        Object o1 = JSONValue.parse(s);
+        JSONObject jsonObj = (JSONObject) o1;
+        String name1 = (String) jsonObj.get("1");
+        String name2 = (String) jsonObj.get("2");
+        String name3 = (String) jsonObj.get("3");
+        String name4 = (String) jsonObj.get("4");
+        String name5 = (String) jsonObj.get("5");
+        System.out.println(name1 + " | " + name2 + " | " + name3 + " | " + name4 +  " | " + name5);
     }
-    
+
+}
+
 }
