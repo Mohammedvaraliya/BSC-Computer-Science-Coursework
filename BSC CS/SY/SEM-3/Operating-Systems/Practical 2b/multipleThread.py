dd87cfc91ff998841f8c5a8a387306cb7f27ed33
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-05 15:11:18 +0530

    Add OS practical 2b

--- BSC CS/SY/SEM-3/Operating-Systems/Practical 2b/multipleThread.py
+++ BSC CS/SY/SEM-3/Operating-Systems/Practical 2b/multipleThread.py
@@ -1,16 +1,21 @@
 import time
 import threading
 
-def myfunc(name):
-    print(f"myfunc starded with {name}")
+def myfunc1(name):
+    print(f"myfunc1 starded with {name}")
     time.sleep(10)
-    print("myfunc ended")
+    print("myfunc1 ended")
+
+def myfunc2(name):
+    print(f"myfunc2 starded with {name}")
+    time.sleep(10)
+    print("myfunc2 ended")
 
 if __name__ == '__main__':
     print("main started")
-    t1 = threading.Thread(target=myfunc, args=["Varaliya"])
+    t1 = threading.Thread(target=myfunc1, args=["Varaliya"])
     t1.start()
-    t2 = threading.Thread(target=myfunc, args=["Mohd"])
+    t2 = threading.Thread(target=myfunc2, args=["Mohd"])
     t2.start()
     t1.join()
     t2.join()
