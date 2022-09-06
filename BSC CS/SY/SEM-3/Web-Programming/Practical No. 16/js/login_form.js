4d9c57cbb6c74142dfb000143dac744829d32c90
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-06 20:41:52 +0530

    web programming practical 16

--- BSC CS/SY/SEM-3/Web-Programming/Practical No. 16/js/login_form.js
+++ BSC CS/SY/SEM-3/Web-Programming/Practical No. 16/js/login_form.js
@@ -3,21 +3,33 @@ let attempt = 3;
 function validate() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
+    let successMessage = document.getElementById('successMessage');
 
-    let passwordChecking = /^[a-zA-Z0-9!@#$%^&*]{4, 8}$/;
+    let passwordChecking = /^[a-zA-Z0-9!@#$%^&*]{4,8}$/;
     if (passwordChecking.test(password) && username != "") {
         alert("Login successfully");
+        successMessage.innerHTML = "You have Logged in successfully";
     }
 
     else {
         attempt--;
         alert("You have left " + attempt + " attempt;");
-        
-        if (attempt == 0) {
+        if (attempt == 2){
+            successMessage.innerHTML = "You have left with 2 attempts";
+        }
+
+        else if(attempt == 1){
+            successMessage.innerHTML = "You have left with 1 attempts";
+        }
+
+        else if (attempt == 0) {
+            successMessage.innerHTML = "Sorry you've reache your limit...";
             document.getElementById("username").disabled = true;
             document.getElementById("password").disabled = true;
             document.getElementById("submit").disabled = true;
             return false;
         }
     }
+
+
 }
