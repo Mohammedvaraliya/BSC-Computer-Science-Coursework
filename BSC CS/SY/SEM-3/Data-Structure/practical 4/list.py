8c89a3958813ffae2dae70a7e765642298728045
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-09 12:22:45 +0530

    Add DS pract 4

---
+++ BSC CS/SY/SEM-3/Data-Structure/practical 4/list.py
@@ -0,0 +1,36 @@
+# List implementation in python
+
+# Creating a List
+
+def create_list():
+    List = []
+    return List
+
+
+# Creating an empty List
+def check_empty(List):
+    return len(List) == 0
+
+
+# Adding items into the List
+def insert(List, i, item):
+    List.insert(i, item)
+    print("Insert item: " , item)
+
+
+# Removing an element from the List
+def remove(List, item):
+    if (check_empty(List)):
+        return "List is empty"
+
+    return List.remove(item)
+
+
+List = create_list()
+insert(List, 0, 1)
+insert(List, 1, 2)
+insert(List, 2, 3)
+insert(List, 3, 4)
+print("Removing item ")
+remove(List, 2)
+print("List after removing an element: " , List)
