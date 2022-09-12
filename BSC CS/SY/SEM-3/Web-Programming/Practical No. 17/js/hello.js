82c6d456a8016edf9e5b08e937e1293658477ede
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-12 15:42:21 +0530

    commit

---
+++ BSC CS/SY/SEM-3/Web-Programming/Practical No. 17/js/hello.js
@@ -0,0 +1,27 @@
+function formValidation() {
+    var uid = document.registration.userid;
+    var passid = document.registration.passid;
+    var uname = document.registration.username;
+    var uadd = document.registration.address;
+    var ucountry = document.registration.country;
+    var uzip = document.registration.zip;
+    var uemail = document.registration.email;
+    var umsex = document.registration.msex;
+    var ufsex = document.registration.fsex; if (userid_validation(uid, 5, 12)) {
+        if (passid_validation(passid, 7, 12)) {
+            if (allLetter(uname)) {
+                if (alphanumeric(uadd)) {
+                    if (countryselect(ucountry)) {
+                        if (allnumeric(uzip)) {
+                            if (ValidateEmail(uemail)) {
+                                if (validsex(umsex, ufsex)) {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
--- BSC CS/SY/SEM-3/Web-Programming/Practical No. 17/js/registration_form.js
+++ BSC CS/SY/SEM-3/Web-Programming/Practical No. 17/js/registration_form.js
@@ -1,22 +1,64 @@
-function validate() {
-    let name = document.getElementById('name').value;
-    let age = document.getElementById('age').value;
-    let email = document.getElementById('email').value;
-    let dateOfBirth = document.getElementById('dateOfBirth').value;
-    let hobbies = document.getElementById('hobbies').value;
-    // let phoneOptions = document.getElementById('phoneOptions').value;
-    let phone = document.getElementById('phone').value;
-    let address = document.getElementById('address').value;
-    // let countryOptions = document.getElementById('countryOptions').value;
-    let service = document.getElementById('service').value;
+// function validate() {
+//     if (allLetter(name)) {
+//         if (alphanumeric(address)) {
 
-    if (isNaN(age)){
-        alert("Please write a valid age");
+//         }
+
+//     }
+
+
+//     if (isNaN(age)) {
+//         alert("Please write a valid age");
+//     }
+
+//     else if (isNaN(phone)) {
+//         alert("Please write a valid phone number");
+//     }
+
+
+// }
+
+let name = document.getElementById('name').value;
+let age = document.getElementById('age').value;
+let email = document.getElementById('email').value;
+let dateOfBirth = document.getElementById('dateOfBirth').value;
+let hobbies = document.getElementById('hobbies').value;
+// let phoneOptions = document.getElementById('phoneOptions').value;
+let phone = document.getElementById('phone').value;
+let address = document.getElementById('address').value;
+// let countryOptions = document.getElementById('countryOptions').value;
+let service = document.getElementById('service').value;
+
+function allLetter() {
+    let letters = /^[A-Za-z]+$/;
+    if (name.value == "") {
+        alert('Username must have not empty');
     }
-
-    else if (isNaN(phone)){
-        alert("Please write a valid phone number");
+    else if (name.value.match(letters)) {
+        return true;
     }
-
-
+    else {
+        alert('Username must have alphabet characters only');
+        name.focus();
+        return false;
+    }
 }
+
+function age() {
+    let age = document.getElementById('age').value;
+    if (isNaN(age)) {
+         alert("Please write a valid age");
+    }       
+}
+
+// function alphanumeric(uadd) {
+//     var letters = /^[0-9a-zA-Z]+$/;
+//     if (uadd.value.match(letters)) {
+//         return true;
+//     }
+//     else {
+//         alert('User address must have alphanumeric characters only');
+//         uadd.focus();
+//         return false;
+//     }
+// }
