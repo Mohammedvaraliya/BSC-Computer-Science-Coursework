7636ab050468867ebfa1be19d8c449d25ecb4b99
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-13 16:40:51 +0530

    Web programming practical 18

---
+++ BSC CS/SY/SEM-3/Linear-Algebra/practical 5/echelon.py
@@ -0,0 +1,14 @@
+# import sympy
+import sympy
+
+# find the reduced row echelon form
+M=sympy.Matrix([[0,1,2,0,3],
+                [2,4,8,2,4],
+                [1,2,4,2,2],
+                [1,3,6,1,5]]).rref()
+
+print("Matrix : {} ".format(M))
+ 
+# find the rank of matrix
+print("Rank of matrix :",sympy.Matrix
+      ([[0,1,2,0,3],[2,4,8,2,4],[1,2,4,2,2],[1,3,6,1,5]]).rank())
