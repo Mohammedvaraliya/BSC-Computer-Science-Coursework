5cb3a4244bcb960b7180019e75d84fa4d96c2a30
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-15 16:55:55 +0530

    OS practical 10 File_System

--- BSC CS/SY/SEM-3/Operating-Systems/Practical-10/file_system.py
+++ BSC CS/SY/SEM-3/Operating-Systems/Practical-10/file_system.py
@@ -2,17 +2,50 @@ import os
 
 print("File System")
 print("\n")
-print("1. create a file")
-print("2. write in a file")
-print("3. Read from file ")
-print("4. Delete a file")
+print("1. Create a file")
+print("2. Write in a file")
+print("3. Append in a file")
+print("4. Read from file ")
+print("5. Delete a file")
+print("\n")
 
-a = int(input("Enter a number"))
+inputNo = int(input("Enter a number : "))
 
-if (a == 1):
-    file = open("C:\\Users\\varal\\Documents\\readme.txt", "a+")
+if (inputNo == 1):
+    input_fname = str(input("Please give a file name :  "))
+    file = open(f"D:\\Practicals Performed\\BSC CS\\SY\\SEM-3\\Operating-Systems\\Practical-10\\Files Folder\\{input_fname}.txt", "a")
     file.close()
-    print("File crated successfully")
-elif(a == 2):
-    file = open("C:\\Users\\varal\\Documents\\readme.txt", "w+")
-    
+    print("File crated successfully.")
+
+elif(inputNo == 2):
+    input_fname = str(input("Please give a file name :  "))
+    input_Str = str(input("Please describe what you write in a file :  "))
+    file = open(f"D:\\Practicals Performed\\BSC CS\\SY\\SEM-3\\Operating-Systems\\Practical-10\\Files Folder\\{input_fname}.txt", "w")
+    file.write(input_Str)
+    file.close()
+    print("Successfully written in a file.")
+
+
+elif(inputNo == 3):
+    input_fname = str(input("Please give a file name :  "))
+    input_Str = str(input("Please describe what you want to append in a file :  "))
+    file = open(f"D:\\Practicals Performed\\BSC CS\\SY\\SEM-3\\Operating-Systems\\Practical-10\\Files Folder\\{input_fname}.txt", "a")
+    file.write(input_Str)
+    file.close()
+    print("Successfully append in a file.")
+
+elif(inputNo == 4):
+    input_fname = str(input("Please give a file name :  "))
+    file = open(f"D:\\Practicals Performed\\BSC CS\\SY\\SEM-3\\Operating-Systems\\Practical-10\\Files Folder\\{input_fname}.txt", "r")
+    content = file.readline()
+    print(content)
+    file.close()
+    print("Successfully read the file.")
+
+elif(inputNo == 5):
+    inputFile = str(input("Please enter file name which you want to delete : "))
+    os.remove(f"D:\\Practicals Performed\\BSC CS\\SY\\SEM-3\\Operating-Systems\\Practical-10\\Files Folder\\{inputFile}.txt")
+    print("Successfully delete the file.")
+
+elif(inputNo > 5):
+    print("Please enter a valid input ")
