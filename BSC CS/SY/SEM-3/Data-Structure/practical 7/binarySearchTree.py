24683775eac72545709e6879cf205e7c964d0958
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-17 19:20:12 +0530

    Add DS pract 7

--- BSC CS/SY/SEM-3/Data-Structure/practical 7/binarySearchTree.py
+++ BSC CS/SY/SEM-3/Data-Structure/practical 7/binarySearchTree.py
@@ -1,78 +1,61 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-
-
-def inorder(root):
-    if root is not None:
-        inorder(root.left)
-        print(str(root.key) + "->", end=' ')
-        inorder(root.right)
-        
-
-def insert(node, key):
-    if node is None:
-        return Node(key)
-
-    if key < node.key:
-        node.left = insert(node.left, key)
-    else:
-        node.right = insert(node.right, key)
-
-    return node
-
-
-def minValueNode(node):
-    current = node
+class BinarySearchTree:
     
-    while(current.left is not None):
-        current = current.left
-    return current
+    @staticmethod
+    def main(args):
+        tree = BST()
+        tree.insert(30)
+        tree.insert(50)
+        tree.insert(15)
+        tree.insert(20)
+        tree.insert(10)
+        tree.insert(40)
+        tree.insert(60)
+        tree.inorder()
 
 
-def deleteNode(root, key):
-    if root is None:
-        return root
+class Node:
+    left = None
+    val = 0
+    right = None
 
-    if key < root.key:
-        root.left = deleteNode(root.left, key)
-        
-    elif(key > root.key):
-        root.right = deleteNode(root.right, key)
-        
-    else:
-        if root.left is None:
-            temp = root.right
-            root = None
-            return temp
-
-        elif root.right is None:
-            temp = root.left
-            root = None
-            return temp
-        temp = minValueNode(root.right)
-        root.key = temp.key
-        root.right = deleteNode(root.right, temp.key)
-        
-    return root
+    def __init__(self, val):
+        self.val = val
 
 
-root = None
-root = insert(root, 8)
-root = insert(root, 3)
-root = insert(root, 1)
-root = insert(root, 6)
-root = insert(root, 7)
-root = insert(root, 10)
-root = insert(root, 14)
-root = insert(root, 4)
+class BST:
+    root = None
 
-print("Inorder traversal: ", end=' ')
-inorder(root)
+    def insert(self, key):
+        node = Node(key)
+        if (self.root == None):
+            self.root = node
+            return
+        prev = None
+        temp = self.root
+        while (temp != None):
+            if (temp.val > key):
+                prev = temp
+                temp = temp.left
+            elif(temp.val < key):
+                prev = temp
+                temp = temp.right
+        if (prev.val > key):
+            prev.left = node
+        else:
+            prev.right = node
 
-print("\nDelete 10")
-root = deleteNode(root, 10)
-print("Inorder traversal: ", end=' ')
-inorder(root)
+    def inorder(self):
+        temp = self.root
+        stack = []
+        while (temp != None or not (len(stack) == 0)):
+            if (temp != None):
+                stack.append(temp)
+                temp = temp.left
+            else:
+                temp = stack.pop()
+                print(str(temp.val) + " ", end="")
+                temp = temp.right
+
+
+if __name__ == "__main__":
+    BinarySearchTree.main([])
