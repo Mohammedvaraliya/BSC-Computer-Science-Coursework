88dd20e83c07209f1a7edabf9f686f64239e848e
MohammedVaraliya <varaliyamohammed2661392@gmail.com>
2022-09-29 00:01:29 +0530

    Add Data Structure practical No 7

--- BSC CS/SY/SEM-3/Data-Structure/practical 7/binarySearchTree.py
+++ BSC CS/SY/SEM-3/Data-Structure/practical 7/binarySearchTree.py
@@ -1,62 +1,91 @@
-class BinarySearchTree:
-    
-    @staticmethod
-    def main(args):
-        tree = BST()
-        tree.insert(30)
-        tree.insert(50)
-        tree.insert(15)
-        tree.insert(20)
-        tree.insert(10)
-        tree.insert(40)
-        tree.insert(60)
-        tree.inorder()
-
-
 class Node:
-    left = None
-    val = 0
-    right = None
 
-    def __init__(self, val):
-        self.val = val
+	def __init__(self, key):
+		self.key = key
+		self.left = None
+		self.right = None
+
+def inorder(root):
+	if root is not None:
+		inorder(root.left)
+		print (root.key,end=" ")
+		inorder(root.right)
+
+def insert(node, key):
+
+	if node is None:
+		return Node(key)
+
+	if key < node.key:
+		node.left = insert(node.left, key)
+	else:
+		node.right = insert(node.right, key)
+	return node
+
+def minValueNode(node):
+	current = node
+
+	while(current.left is not None):
+		current = current.left
+
+	return current
+
+def deleteNode(root, key):
+
+	if root is None:
+		return root
+
+	if key < root.key:
+		root.left = deleteNode(root.left, key)
+	elif(key > root.key):
+		root.right = deleteNode(root.right, key)
+	else:
+		if root.left is None:
+			temp = root.right
+			root = None
+			return temp
+
+		elif root.right is None:
+			temp = root.left
+			root = None
+			return temp
+
+		temp = minValueNode(root.right)
+
+		root.key = temp.key
+
+		root.right = deleteNode(root.right, temp.key)
+
+	return root
 
 
-class BST:
-    root = None
+root = None
+root = insert(root, 50)
+root = insert(root, 30)
+root = insert(root, 20)
+root = insert(root, 40)
+root = insert(root, 70)
+root = insert(root, 60)
+root = insert(root, 80)
 
-    def insert(self, key):
-        node = Node(key)
-        if (self.root == None):
-            self.root = node
-            return
-        prev = None
-        temp = self.root
-        while (temp != None):
-            if (temp.val > key):
-                prev = temp
-                temp = temp.left
-            elif(temp.val < key):
-                prev = temp
-                temp = temp.right
-        if (prev.val > key):
-            prev.left = node
-        else:
-            prev.right = node
+print ("Inorder traversal of the given tree")
+inorder(root)
+print("\n")
 
-    def inorder(self):
-        temp = self.root
-        stack = []
-        while (temp != None or not (len(stack) == 0)):
-            if (temp != None):
-                stack.append(temp)
-                temp = temp.left
-            else:
-                temp = stack.pop()
-                print(str(temp.val) + " ", end="")
-                temp = temp.right
+print ("\nDelete 20")
+root = deleteNode(root, 20)
+print ("Inorder traversal of the modified tree")
+inorder(root)
+print("\n")
 
+print ("\nDelete 30")
+root = deleteNode(root, 30)
+print ("Inorder traversal of the modified tree")
+inorder(root)
+print("\n")
 
-if __name__ == "__main__":
-    
-    BinarySearchTree.main([])
+print ("\nDelete 50")
+root = deleteNode(root, 50)
+print ("Inorder traversal of the modified tree")
+inorder(root)
+
