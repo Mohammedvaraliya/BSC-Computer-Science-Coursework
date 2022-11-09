923db65f9f88a0f6c4d2a2088446f180071fac00
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-09 19:59:33 +0530

    add sem-2 advance python practical 1

--- BSC CS/FY/SEM-2/Advanced Python Programming/practical-1/pract_1_question_2.py
+++ BSC CS/FY/SEM-2/Advanced Python Programming/practical-1/pract_1_question_2.py
@@ -1,4 +1,5 @@
 # WAP to read an existing file.
 
-f = open("hello.txt",'r')
-f.read()
+f = open("D:\\python Projects\\BSC-CS-Practical-Performed\\BSC CS\\FY\SEM-2\\Advanced Python Programming\\practical-1\\hello.txt",'r')
+a = f.read()
+print(a)
