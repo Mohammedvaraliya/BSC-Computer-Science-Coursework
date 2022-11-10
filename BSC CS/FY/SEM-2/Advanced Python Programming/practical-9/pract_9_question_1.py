c07e2e3694e6630c3c29fff41cb79ea4fc62313b
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-10 23:12:19 +0530

    add sem-2 advance python practical 9

---
+++ BSC CS/FY/SEM-2/Advanced Python Programming/practical-9/pract_9_question_1.py
@@ -0,0 +1,7 @@
+# Create an object named p1, and print the value of x:
+
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
---
+++ BSC CS/FY/SEM-2/Advanced Python Programming/practical-9/pract_9_question_2.py
@@ -0,0 +1,12 @@
+# The __init__() Function
+
+class Person:
+  
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Varaliya", 20)
+
+print(p1.name)
+print(p1.age)
