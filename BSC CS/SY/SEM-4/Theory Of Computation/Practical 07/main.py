a93688e61e8a8a3e32ef02e9934c72fe6738b275
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-03-02 19:37:50 +0530

    Add practical 07 theory of computation

--- BSC CS/SY/SEM-4/Theory Of Computation/Practical 07/main.py
+++ BSC CS/SY/SEM-4/Theory Of Computation/Practical 07/main.py
@@ -25,6 +25,19 @@ def count_string(string:str):
     return f"The Number of 1's is {count_1} and number of 0's is {count_0}"
 
 
+def check_string_recursive(string:str, current_state:str, count_0 = 0, count_1 = 0):
+    # print(string, current_state)
+    if len(string) == 0:
+        return f"The Number of 1's is {count_1} and number of 0's is {count_0}"
+    
+    if string[0] == "0":
+        count_0 += 1
+    if string[0] == "1":
+        count_1 += 1 
+
+    return check_string_recursive(string[1:], states[current_state][string[0]], count_0, count_1)
+
+
 
 
 if __name__ == "__main__":
@@ -33,4 +46,7 @@ if __name__ == "__main__":
 
     X = count_string(user_input)
     print(X)
+
+    Y = check_string_recursive(user_input, initial_state)
+    print(Y)
 
