c93eaf6fee90ea3963793b244fbb9ff118afc681
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-08-11 20:49:23 +0530

    Add Game Programming Practical 01 initialization

---
+++ BSC CS/TY/SEM-5/Game Programming/Practical-01/Practical-01/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace Practical_01
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.DirectX.Direct3D.Device device;
+        public Form1()
+        {
+            InitializeComponent();
+            InitDevice();
+        }
+        private void InitDevice()
+        {
+            PresentParameters pp = new PresentParameters(); //CREATE OBJECT
+            pp.Windowed = true;
+            pp.SwapEffect = SwapEffect.Discard;
+            device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing,
+           pp);
+        }
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target, Color.Blue, 0, 1);
+            device.Present();
+        }
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
