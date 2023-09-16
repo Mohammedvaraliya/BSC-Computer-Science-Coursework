8af6e8ce4894e2632967bcfa2059f59a1759cf37
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-09-16 14:31:48 +0530

    Added First Practical INS "Substitution and Transposition Ciphers"

--- BSC CS/TY/SEM-5/Information & Network Security/practical 01/ceaser_cipher.py
+++ BSC CS/TY/SEM-5/Information & Network Security/practical 01/ceaser_cipher.py
@@ -1,21 +1,40 @@
-def encrypt(text,s):
+def encrypt(text, s):
     result = ""
-    
+
     for i in range(len(text)):
         char = text[i]
-        
-        if (char.isupper()):
-            result += chr((ord(char) + s - 65) % 26 + 65) # Encrypt lowercase characters
+
+        if char.isupper():
+            result += chr((ord(char) + s - 65) % 26 + 65)  # Encrypt uppercase characters
+        elif char.islower():
+            result += chr((ord(char) + s - 97) % 26 + 97)  # Encrypt lowercase characters
         else:
-            result += chr((ord(char) + s - 97) % 26 + 97)
+            result += char  # Leave non-alphabet characters unchanged
+
     return result
 
+def decrypt(encrypted_text, s):
+    decrypted_result = ""
 
+    for i in range(len(encrypted_text)):
+        char = encrypted_text[i]
+
+        if char.isupper():
+            decrypted_result += chr(((ord(char) - s - 65) + 26) % 26 + 65)  # Decrypt uppercase characters
+        elif char.islower():
+            decrypted_result += chr(((ord(char) - s - 97) + 26) % 26 + 97)  # Decrypt lowercase characters
+        else:
+            decrypted_result += char  # Leave non-alphabet characters unchanged
+
+    return decrypted_result
 
 if __name__ == "__main__":
-    
-    text=input(" Enter the text to encrypt ")
+    text = input("Enter the text to encrypt: ")
     s = 3
-    print("Text : " + text)
-    str(s)
-    print( "Cipher: " + encrypt(text,s))
+    print("Text: " + text)
+
+    encrypted_text = encrypt(text, s)
+    print("Cipher: " + encrypted_text)
+
+    decrypted_text = decrypt(encrypted_text, s)
+    print("Decrypted Text: " + decrypted_text)
---
+++ BSC CS/TY/SEM-5/Information & Network Security/practical 02/README.md
@@ -0,0 +1,7 @@
+## Practical 02
+
+## Problem Statement
+
+* RSA Encryption and Decryption:
+* Implement the RSA algorithm for public-key encryption and decryption, and explore its properties and security considerations.
+
