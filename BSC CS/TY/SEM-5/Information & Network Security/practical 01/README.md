65505cb761b4fe47a942bb2bed374accda31bc1c
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-09-16 18:08:09 +0530

    Added First Practical INS "Substitution and Transposition Ciphers"

--- BSC CS/TY/SEM-5/Information & Network Security/practical 01/README.md
+++ BSC CS/TY/SEM-5/Information & Network Security/practical 01/README.md
@@ -4,4 +4,3 @@
 
 * Implementing Substitution and Transposition Ciphers:
 * Design and implement algorithms to encrypt and decrypt messages using classical substitution and transposition techniques.
-
---
+++ BSC CS/TY/SEM-5/Information & Network Security/practical 01/rail_fence.py
@@ -0,0 +1,57 @@
+def rail_fence_encrypt(text, rails):
+    fence = [[' ' for _ in range(len(text))] for _ in range(rails)]
+    direction = -1  # Direction to move along the rails (up or down)
+    row, col = 0, 0  # Initial position on the rail fence
+
+    for char in text:
+        fence[row][col] = char
+        if row == 0 or row == rails - 1:
+            direction *= -1  # Change direction at the top or bottom rail
+        row += direction
+        col += 1
+
+    encrypted_text = ''.join([''.join(row) for row in fence]).replace(" ", "")  # Remove spaces
+    return encrypted_text
+
+def rail_fence_decrypt(encrypted_text, rails):
+    fence = [[' ' for _ in range(len(encrypted_text))] for _ in range(rails)]
+    direction = -1  # Direction to move along the rails (up or down)
+    row, col = 0, 0  # Initial position on the rail fence
+
+    for _ in range(len(encrypted_text)):
+        fence[row][col] = 'X'  # Mark the positions on the fence
+        if row == 0 or row == rails - 1:
+            direction *= -1  # Change direction at the top or bottom rail
+        row += direction
+        col += 1
+
+    decrypted_text = ''
+    text_index = 0
+
+    for row in range(rails):
+        for col in range(len(encrypted_text)):
+            if fence[row][col] == 'X':
+                fence[row][col] = encrypted_text[text_index]
+                text_index += 1
+
+    direction = -1  # Reset the direction for reading
+    row, col = 0, 0
+
+    for _ in range(len(encrypted_text)):
+        decrypted_text += fence[row][col]
+        if row == 0 or row == rails - 1:
+            direction *= -1  # Change direction at the top or bottom rail
+        row += direction
+        col += 1
+
+    return decrypted_text
+
+if __name__ == "__main__":
+    text = input("Enter the text to encrypt: ")
+    rails = int(input("Enter the number of rails: "))
+
+    encrypted_text = rail_fence_encrypt(text, rails)
+    print("Cipher: " + encrypted_text)
+
+    decrypted_text = rail_fence_decrypt(encrypted_text, rails)
+    print("Decrypted Text: " + decrypted_text)
