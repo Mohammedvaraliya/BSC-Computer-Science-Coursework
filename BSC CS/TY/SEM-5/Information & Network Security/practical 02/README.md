fd979383c42d62c5591705db4cb58601e38eeb75
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-09-17 11:58:06 +0530

    Added 2nd Practical INS "RSA Encryption and Decryption"

--- BSC CS/TY/SEM-5/Information & Network Security/practical 02/README.md
+++ BSC CS/TY/SEM-5/Information & Network Security/practical 02/README.md
@@ -5,3 +5,10 @@
 * RSA Encryption and Decryption:
 * Implement the RSA algorithm for public-key encryption and decryption, and explore its properties and security considerations.
 
+---
+
+**The Package has to be install**
+
+```bash
+pip install pycryptodome
+```
---
+++ BSC CS/TY/SEM-5/Information & Network Security/practical 02/rsa.py
@@ -0,0 +1,20 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+
+keyPair = RSA.generate(1024)
+
+pubKey = keyPair.publickey()
+print(f"Public key: (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+
+# Encryption
+msg = b'MVLU College'  # Convert the message to bytes
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:", binascii.hexlify(encrypted).decode('ascii'))
