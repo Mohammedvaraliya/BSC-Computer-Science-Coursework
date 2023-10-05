c7f2ebfaa50320ec42f74d624ed3574c73e5035e
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-10-05 17:58:46 +0530

    Updated Pract GP 02

--- BSC CS/TY/SEM-5/Game Programming/Practical 02/pygame_draw_shapes.py
+++ BSC CS/TY/SEM-5/Game Programming/Practical 02/pygame_draw_shapes.py
@@ -12,43 +12,20 @@ while not done:
     # clock.tick() limits the while loop to a max of 10 times per second.  
     clock.tick(10)
   
-    for event in pygame.event.get():  # User did something  
-        if event.type == pygame.QUIT:  # If user clicked on close symbol   
-            done = True  # done variable that we are complete, so we exit this loop  
-
-    # All drawing code occurs after the for loop and but  
-    # inside the main while done==False loop.  
-
-    # Clear the default screen background and set the white screen background  
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            done = True 
     screen.fill((0, 0, 0))  
 
-    # Draw on the screen a green line which is 5 pixels wide.  
     pygame.draw.line(screen, (0, 255, 0), [0, 0], [50, 30], 5)  
-    # Draw on the screen a green line which is 5 pixels wide.  
-    pygame.draw.lines(screen, (0, 0, 0), False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5)  
-
-    # Draw a rectangle outline  
-    pygame.draw.rect(screen, (0, 0, 0), [75, 10, 50, 20], 2)  
-
-    # Draw a solid rectangle  
-    pygame.draw.rect(screen, (0, 0, 0), [150, 10, 50, 20])  
-
-    # This draw an ellipse outline, using a rectangle as the outside boundaries  
-    pygame.draw.ellipse(screen, (255, 0, 0), [225, 10, 50, 20], 2)  
-
-    # This draw a solid ellipse, using a rectangle as the outside boundaries  
-    pygame.draw.ellipse(screen, (255, 0, 0), [300, 10, 50, 20])  
-
-    # Draw a triangle using the polygon function  
-    pygame.draw.polygon(screen, (0, 0, 0), [[100, 100], [0, 200], [200, 200]], 5)  
-
-    # This draw a circle  
-    pygame.draw.circle(screen, (0, 0, 255), [60, 250], 40)  
-
-    # This draw an arc  
-    pygame.draw.arc(screen, (0, 0, 0), [210, 75, 150, 125], 0, pi / 2, 2)  
-
-    # This function must write after all the other drawing commands.  
+    pygame.draw.lines(screen, (0, 0, 0), False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5) 
+    pygame.draw.rect(screen, (0, 0, 0), [75, 10, 50, 20], 2)
+    pygame.draw.rect(screen, (0, 0, 0), [150, 10, 50, 20])
+    pygame.draw.ellipse(screen, (255, 0, 0), [225, 10, 50, 20], 2)
+    pygame.draw.ellipse(screen, (255, 0, 0), [300, 10, 50, 20])
+    pygame.draw.polygon(screen, (0, 0, 0), [[100, 100], [0, 200], [200, 200]], 5)
+    pygame.draw.circle(screen, (0, 0, 255), [60, 250], 40)
+    pygame.draw.arc(screen, (0, 0, 0), [210, 75, 150, 125], 0, pi / 2, 2)
     pygame.display.flip()  
   
 # Quite the execution when clicking on close  
