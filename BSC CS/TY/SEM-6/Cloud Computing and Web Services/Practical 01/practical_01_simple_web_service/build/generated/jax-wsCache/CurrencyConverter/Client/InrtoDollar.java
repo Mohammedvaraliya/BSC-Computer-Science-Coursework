deaff6b279e8de5c5493b0f1f6989076f03c4c86
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2024-02-05 15:04:11 +0530

    Added Practical 01 of Cloud Computing and Web Services

---
+++ BSC CS/TY/SEM-6/Cloud Computing and Web Services/Practical 01/practical_01_simple_web_service/build/generated/jax-wsCache/CurrencyConverter/Client/InrtoDollar.java
@@ -0,0 +1,52 @@
+
+package Client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for InrtoDollar complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="InrtoDollar">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="a" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "InrtoDollar", propOrder = {
+    "a"
+})
+public class InrtoDollar {
+
+    protected double a;
+
+    /**
+     * Gets the value of the a property.
+     * 
+     */
+    public double getA() {
+        return a;
+    }
+
+    /**
+     * Sets the value of the a property.
+     * 
+     */
+    public void setA(double value) {
+        this.a = value;
+    }
+
+}
